7524b9c14c968b7f1e4cb4c3f596cb67141a8c88
MD HADIUZZAMAN ZAFRI <mdhadiuzzamanzafri@zafri.local>
2024-06-16 00:15:05 +0530

    update v2 of the app, added the mongoDB, working with mongoose

--- app.js
+++ app.js
@@ -2,17 +2,42 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose"); 
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItem = [];
+
+// I comment it out because of now i am going to use mongoose
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
+// let workItem = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs"); 
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model( "Item", itemsSchema);
+
+const item1 = new Item({
+    name : "Welcome to todo list"
+});
+const item2 = new Item({
+    name : "Hit the + button to add a new item"
+});
+const item3 = new Item({
+    name : "<-- Hit this to delete an item>"
+});
+
+const defaultItems = [ item1, item2, item3]; 
+
+
 
 app.get("/", function(req, res){
     // res.write("Hello");
@@ -26,9 +51,31 @@ app.get("/", function(req, res){
 
     // var day = today.toLocaleDateString("en-US", options);
 
-    let day = date();
+    // let day = date();
 
-    res.render("list", {listTitle: day, newItem: items});
+    Item.find()
+      .then(function (results) {   
+
+        if(results.length === 0) {
+            Item.insertMany(defaultItems)
+                .then(function () {
+                    console.log("Successfully saved defult items to DB");
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+                res.redirect("/");
+        }else{
+            res.render("list", {listTitle: "Today", newItem: results}); 
+        }
+
+        
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+    
     
 });
 
